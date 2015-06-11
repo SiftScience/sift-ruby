2c010bf0f9297721846dae141326e396d837688d
Fred Sadaghiani <fred.sadaghiani@gmail.com>
2015-06-11 16:18:14 -0700

    Add tests and clean up code from PR review

--- examples/device.rb
+++ examples/device.rb
@@ -12,28 +12,28 @@ account_id = ENV['ACCOUNT_ID']
 
 
 if api_key.nil? || account_id.nil?
-  puts "Please specify the environment variables API_KEY and API_ID"
+  puts "Please specify the environment variables API_KEY and ACCOUNT_ID"
   puts "usage:"
-  puts " API_KEY=xxxxxx API_ID=xxxxxxxx ruby device_id.rb"
+  puts " API_KEY=xxxxxx ACCOUNT_ID=xxxxxxxx ruby device_id.rb"
   puts
-  exit
+  exit 1
 end
 
 class MyLogger
   def warn(e)
-    puts "[WARN]  " + e.to_s
+    $stderr.puts "[WARN]  " + e.to_s
   end
 
   def error(e)
-    puts "[ERROR] " + e.to_s
+    $stderr.puts "[ERROR] " + e.to_s
   end
 
   def fatal(e)
-    puts "[FATAL] " + e.to_s
+    $stderr.puts "[FATAL] " + e.to_s
   end
 
   def info(e)
-    puts "[INFO]  " + e.to_s
+    $stderr.puts "[INFO]  " + e.to_s
   end
 end
 
@@ -43,25 +43,36 @@ Sift.account_id = account_id
 
 def print_device(d)
   puts 'i got the device obj: ' + d.inspect
+  puts 'device first_seen ' + d.first_seen.to_s
   puts 'device bad? ' + d.bad?.to_s
   puts 'device labeled? ' + d.labeled?.to_s
   puts 'device labeled_at ' + d.labeled_at.to_s
   puts 'device session timestamps ' + d.session_timestamps.join(',')
 end
 
-device = Sift::Device.retrieve("i08a78q46cekukrugj7ltnqskj")
-if device
-  print_device(device)
-end
+begin
 
-session = Sift::Session.retrieve("session_id")
-if session
-  print_device(session.device)
-  current_label = session.device.label_bad!
+  device_id = ENV['DEVICE_ID'] || raise("DEVICE_ID env variable not specified")
+  session_id = ENV['SESSION_ID'] || raise("SESSION_ID env variable not specified")
 
-  puts 'got label from API result: ' + current_label.to_s
-  puts 'session device label ' + session.device.label
+  device = Sift::Device.retrieve(device_id)
+  if device
+    print_device(device)
+  end
 
+  session = Sift::Session.retrieve(session_id)
+  if session
+    print_device(session.device)
+    current_label = session.device.label_bad!
+
+    puts 'got label from API result: ' + current_label.to_s
+    puts 'session device label ' + session.device.label
+
+  end
+rescue Sift::HttpRequestError => e
+  $stderr.puts e.http_status
+  $stderr.puts e.body
+  $stderr.puts $!
 end
 
 exit 0
--- lib/sift/network.rb
+++ lib/sift/network.rb
@@ -3,13 +3,18 @@ module Sift
   module NetworkRepresenter
     include Representable::JSON
     include Representable::Object
-    property :first_seen
+    property :internal_first_seen, as: :first_seen
     property :score
   end
 
-  class Network < Struct.new :first_seen, :score
+  class Network < Struct.new :internal_first_seen, :score
     def risky?
-      self.score > 0.5
+      self.score >= 0.7
+    end
+
+    def first_seen
+      # NB. The API returns timestamps in millis since the Epoch
+      Time.at(self.internal_first_seen / 1000) if self.internal_first_seen
     end
   end
 
