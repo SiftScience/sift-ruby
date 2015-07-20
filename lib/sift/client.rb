6ed9a27b24c8308973bd63a7bda9e43327ef33b8
Mauricio Murga <mauricio@conekta.io>
2015-07-20 15:12:48 -0500

    integrate return_action logic to sift client

--- lib/sift/client.rb
+++ lib/sift/client.rb
@@ -132,15 +132,18 @@ module Sift
     #   the status message and status code. In general, you can ignore the returned
     #   result, though.
     #
-    def track(event, properties = {}, timeout = nil, path = nil, return_score = false, api_key = @api_key)
+    def track(event, properties = {}, timeout = nil, path = nil, return_score = false, return_action = false, api_key = @api_key)
       raise("event must be a non-empty string") if (!event.is_a? String) || event.empty?
       raise("properties cannot be empty") if properties.empty?
       raise("Bad api_key parameter") if api_key.empty?
       path ||= @path
       timeout ||= @timeout
-      if return_score
-        path = path + "?return_score=true"
-      end
+
+      uri = URI.parse(API_ENDPOINT)      
+      uri.query = URI.encode_www_form(URI.decode_www_form(uri.query.to_s) << ["return_score", "true"]) if return_score
+      uri.query = URI.encode_www_form(URI.decode_www_form(uri.query.to_s) << ["return_action", "true"]) if return_action
+      path = path + "?" + uri.query if !uri.query.to_s.empty?
+
       options = {
         :body => MultiJson.dump(delete_nils(properties).merge({"$type" => event,
                                                                "$api_key" => api_key})),
@@ -237,6 +240,9 @@ module Sift
     end
 
     private
+    # def add_query_parameter(query_parameter)
+    #   uri = URI.parse(API_ENDPOINT)
+    # end
     def delete_nils(properties)
       properties.delete_if do |k, v|
         case v
--- spec/integration/sift_spec.rb
+++ spec/integration/sift_spec.rb
@@ -2,6 +2,62 @@ require File.expand_path(File.join(File.dirname(__FILE__), "..", "spec_helper"))
 
 describe Sift do
 
+  it "test action response" do
+    Sift.api_key = 'd86ef3777aa34729'
+    client = Sift::Client.new()
 
+    # send a transaction event -- note this is blocking 
+    event = "$transaction"
+
+    user_id = "23056" # User ID's may only contain a-z, A-Z, 0-9, =, ., -, _, +, @, :, &, ^, %, !, $
+
+    properties = {
+     "$user_id" => user_id, 
+     "$user_email" => "buyer@gmail.com", 
+     "$seller_user_id" => "2371", 
+     "seller_user_email" => "seller@gmail.com", 
+     "$transaction_id" => "573050", 
+     "$payment_method" => {
+      "$payment_type"    => "$credit_card",
+      "$payment_gateway" => "$braintree",
+      "$card_bin"        => "542486",
+      "$card_last4"      => "4444"             
+      },
+      "$currency_code" => "USD",
+      "$amount" => 15230000,
+    }
+
+    response = client.track(event, properties, nil, nil, true)
+    puts response.inspect
+  end
+
+  it "test score response" do
+    Sift.api_key = 'd86ef3777aa34729'
+    client = Sift::Client.new()
+
+    # send a transaction event -- note this is blocking 
+    event = "$transaction"
+
+    user_id = "23056" # User ID's may only contain a-z, A-Z, 0-9, =, ., -, _, +, @, :, &, ^, %, !, $
+
+    properties = {
+     "$user_id" => user_id, 
+     "$user_email" => "buyer@gmail.com", 
+     "$seller_user_id" => "2371", 
+     "seller_user_email" => "seller@gmail.com", 
+     "$transaction_id" => "573050", 
+     "$payment_method" => {
+      "$payment_type"    => "$credit_card",
+      "$payment_gateway" => "$braintree",
+      "$card_bin"        => "542486",
+      "$card_last4"      => "4444"             
+      },
+      "$currency_code" => "USD",
+      "$amount" => 15230000,
+    }
+
+    response = client.track(event, properties, nil, nil, nil, true)
+    puts response.inspect
+  end
 
 end
