9ba63abb6b4a518157c61fe30d5e36b2eb2d887d
vterlyha <vterlyha@siftscience.com>
2025-04-04 16:25:04 +0300

    ATO-4761: Fix Verification URLs in sift-ruby

--- test_integration_app/verification_api/test_verification_api.rb
+++ test_integration_app/verification_api/test_verification_api.rb
@@ -29,38 +29,4 @@ class VerificationAPI
         return @@client.verification_send(properties)
     end
 
-    def resend()
-        properties = {
-            "$user_id"            => $user_id,
-            "$send_to"            => $user_email,
-            "$verification_type"            => "$email",
-            "$brand_name"         => "MyTopBrand",
-            "$language"           => "en",
-            "$site_country" => "IN",
-            "$event" => {
-                "$session_id" => "SOME_SESSION_ID",
-                "$verified_event" => "$login",
-                "$verified_entity_id" => "SOME_SESSION_ID",
-                "$reason" => "$automated_rule",
-                "$ip" => "192.168.1.1",
-                "$browser" => {
-                    "$user_agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
-                    "$accept_language" => "en-US",
-                    "$content_language" => "en-GB"
-                }
-           }
-        }
-
-        return @@client.verification_resend(properties)
-    end
-
-    def check()
-        properties = {
-            "$user_id"            => $user_id,
-            "$code"               => "123456",
-        }
-
-        return @@client.verification_check(properties)
-    end
-    
 end
